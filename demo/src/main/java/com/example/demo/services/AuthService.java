ede66d1673044c5f547d04ca11d3a385775c8c8e
sandhyaspks <sandhya.s2023ece@sece.ac.in>
2025-07-09 16:04:21 +0530

    Day6Aft

--- demo/src/main/java/com/example/demo/services/AuthService.java
+++ demo/src/main/java/com/example/demo/services/AuthService.java
@@ -3,6 +3,7 @@ package com.example.demo.services;
 import com.example.demo.models.RegisterDetails;
 import com.example.demo.repository.RegisterDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -11,14 +12,16 @@ import java.util.Objects;
 public class AuthService {
     @Autowired
     RegisterDetailsRepository registerDetailsRepository;
-
+    @Autowired
+    PasswordEncoder passwordEncoder;
     public String addNewEmployee(RegisterDetails register) {
         RegisterDetails registerDetails = new RegisterDetails();
         registerDetails.setEmpId(register.getEmpId());
         registerDetails.setEmail(register.getEmail());
         registerDetails.setGender(register.getGender());
         registerDetails.setRole(register.getRole());
-        registerDetails.setPassword(register.getPassword());
+        System.out.println("Password is: " + register.getPassword()+ "\n Encrypted Password is "+ passwordEncoder.encode(register.getPassword()));
+        registerDetails.setPassword(passwordEncoder.encode(register.getPassword()));
         registerDetails.setEmpname(register.getEmpname());
         registerDetails.setDateOfBirth(register.getDateOfBirth());
         registerDetailsRepository.save(registerDetails);
@@ -28,13 +31,13 @@ public class AuthService {
     public String authenticate(RegisterDetails login) {
         RegisterDetails user = registerDetailsRepository.findByEmail(login.getEmail());
         if (user != null) {
-            if (Objects.equals(user.getPassword(), login.getPassword())) {
+            if (passwordEncoder.matches(login.getPassword(),user.getPassword())) {
                 return "Login Successful";
             } else {
-                return "Login not successfull";
+                return "Login not successful";
             }
         } else {
-            return "Login not successfull";
+            return "Login not successful";
 
         }
     }
